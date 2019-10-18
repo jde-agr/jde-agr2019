c31e5b60d5c04d7019ddf7aceb59901a6185cc43
jde-agr <jde-agr@student.wethinkcode.co.za>
2019-10-18 09:55:06 +0200

    Initial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/Pods-jde-agr2019_Tests/Pods-jde-agr2019_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_jde_agr2019_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_jde_agr2019_Tests
+@end
---
+++ Example/Pods/Target Support Files/jde-agr2019/jde-agr2019-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_jde_agr2019 : NSObject
+@end
+@implementation PodsDummy_jde_agr2019
+@end
